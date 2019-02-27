950c70db18264f5aaefdf980dd305999c101a127
oleksii-lyzun <lyzun@semalt.com>
2019-02-27 18:54:03 +0200

    addToShip function added

--- src/components/BattleFieldUser.vue
+++ src/components/BattleFieldUser.vue
@@ -18,6 +18,7 @@ import BattleFieldUserSquare from './BattleFieldUserSquare.vue';
 // Mixins and helpers
 import BattleFieldMixin from '../mixins/BattleFieldMixin.js';
 import Numbers from '../helpers/Numbers';
+import Arrays from '../helpers/Arrays';
 
 export default {
 	name: "BattleFieldUser",
@@ -41,10 +42,39 @@ export default {
 		return {
 			// import from BattleFieldMixin presented
 
+			allowedSquares: [-10, -1, +1, +10],
+			allowedSquaresZeros: [-10, +1, +10],
+			allowedSquaresNines: [-10, -1, +10],
+
 			forbiddenSquares: [-11, -9, +9, +11],
 			forbiddenSquaresZeros: [-9, +11],
 			forbiddenSquaresNines: [-11, +9],
 		}
+	},
+	computed: {
+		shipsCnt() {
+			let cnt = {one: 0, two: 0, three: 0, four: 0};
+			this.ships.map(ship => {
+				switch (ship.length) {
+					case 1:
+						cnt.one++;
+						break;
+					case 2:
+						cnt.two++;
+						break;
+					case 3:
+						cnt.three++;
+						break;
+					case 4:
+						cnt.four++;
+						break;
+				}
+			});
+			return cnt;
+		},
+		totalShipsCnt() {
+			return this.ships.length;
+		},
 	},
 	methods: {
 		squareWasClicked(status) {
@@ -57,7 +87,109 @@ export default {
 			let diagonalClick = this.checkDiagonal(squareNumber);
 			if (diagonalClick) return;
 
+			this.updateShips(squareNumber, clickedStatus);
+
 			this.setMarkedStatus(squareNumber, clickedStatus);
+		},
+		updateShips(squareNumber, clickedStatus) {
+			if (clickedStatus) {
+				this.addToShip(squareNumber, clickedStatus);
+			}
+		},
+		addToShip(squareNumber) {
+			let cnt = [];
+			let parts = null;
+			let isAdded = null;
+
+			if (squareNumber % 10 === 0) {
+				parts = this.allowedSquaresZeros.map(el => squareNumber + (el)).filter(el => el > -1 && el < 100);
+			} else if (squareNumber % 10 === 9) {
+				parts = this.allowedSquaresNines.map(el => squareNumber + (el)).filter(el => el > -1 && el < 100);
+			} else {
+				parts = this.allowedSquares.map(el => squareNumber + (el)).filter(el => el > -1 && el < 100);
+			}
+
+			this.ships.map((ship, index) => {
+				ship.map(square => {
+					if (parts.indexOf(square) !== -1) {
+						cnt.push(index);
+					}
+				})
+			});
+
+			if (cnt.length === 2) {
+				isAdded = this.glueTwoShips(cnt, squareNumber);
+			} else {
+				isAdded = this.extendOneShip(parts, squareNumber);
+			}
+
+			if (isAdded === null && this.totalShipsCnt < 10) {
+				this.ships = [...this.ships, [squareNumber]];
+				isAdded = true;
+			} else {
+				isAdded = false;
+			}
+
+			console.log('this.ships', this.ships);
+
+			return isAdded;
+		},
+		glueTwoShips(cnt, squareNumber) {
+			let isAdded = null;
+
+			let newShip = [...this.ships[cnt[0]], ...this.ships[cnt[1]]];
+
+			if (newShip.length === 4) isAdded = false;
+			if (newShip.length === 3 && this.shipsCnt.four === 0) {
+				isAdded = true;
+				this.ships = Arrays.removeValues(this.ships, cnt);
+				this.ships.push([...newShip, squareNumber]);
+			}
+			if (newShip.length === 2 && this.shipsCnt.three < 2) {
+				isAdded = true;
+				this.ships = Arrays.removeValues(this.ships, cnt);
+				this.ships.push([...newShip, squareNumber]);
+			}
+			if (newShip.length === 1 && this.shipsCnt.two < 3) {
+				isAdded = true;
+				this.ships = Arrays.removeValues(this.ships, cnt);
+				this.ships.push([...newShip, squareNumber]);
+			}
+
+			return isAdded;
+		},
+		extendOneShip(parts, squareNumber) {
+			let isAdded = null;
+
+			for (let i = 0; i < this.ships.length; i++)
+			{
+				for (let j = 0; j < this.ships[i].length; j++)
+				{
+					let square = this.ships[i][j];
+					if (parts.indexOf(square) !== -1) {
+						let shipLength = this.ships[i].length;
+
+						if (shipLength === 4) isAdded = false;
+						if (shipLength === 3 && this.shipsCnt.four === 0) {
+							isAdded = true;
+							this.ships[i].push(squareNumber);
+						}
+						if (shipLength === 2 && this.shipsCnt.three < 2) {
+							isAdded = true;
+							this.ships[i].push(squareNumber);
+						}
+						if (shipLength === 1 && this.shipsCnt.two < 3) {
+							isAdded = true;
+							this.ships[i].push(squareNumber);
+						}
+					}
+				}
+			}
+
+			return isAdded;
+		},
+		deleteFromShip() {
+			console.log('Not ready');
 		},
 		setMarkedStatus(square, clickedStatus) {
 			if (clickedStatus) {
