10d04b2ae4af24c16237b6de9cbb37bd63e5f813
oleksii-lyzun <lyzun@semalt.com>
2019-02-27 18:53:24 +0200

    Arrays.removeValues added

--- src/helpers/Arrays.js
+++ src/helpers/Arrays.js
@@ -3,4 +3,12 @@ export default class Arrays
 	static unique(arr) {
 		return [...new Set(arr)];
 	}
+
+	static removeValues(arr, idx) {
+		for (let i = idx.length -1; i >= 0; i--)
+		{
+			arr.splice(idx[i], 1);
+		}
+		return arr;
+	}
 }
